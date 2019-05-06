02e9d57e0bc874f2f904f078b0495d4e91cf8c2f
Safi <sfmissive@gmail.com>
2019-05-06 23:02:05 +0300

    ⚗ Refactor to allow manual build and release creation.

--- source/script.js
+++ source/script.js
@@ -1,16 +1,16 @@
-export function decorator({ 
-    /** mixin complies with `Mixin` function of the 'mixwith' module. 
-      * e.g. Mixin({ Superclass: class{} } => class Y extends Superclass {})
-      */
-    mixin = null 
+export function decorator({
+  /** mixin complies with `Mixin` function of the 'mixwith' module.
+   * e.g. Mixin({ Superclass: class{} } => class Y extends Superclass {})
+   */
+  mixin = null,
 }) {
-    return Class => {
-        // add controller methods for the specific module that uses them.
-        if(mixin) {
-            Class = mixin({ Superclass: Class }) /* return Specific implementation Controller */
-        } // else return Reusable nested unit 
-        return Class
-    }
+  return Class => {
+    // add controller methods for the specific module that uses them.
+    if (mixin) {
+      Class = mixin({ Superclass: Class }) /* return Specific implementation Controller */
+    } // else return Reusable nested unit
+    return Class
+  }
 }
 
 export * from 'mixwith'
--- test/entrypoint.test.js
+++ test/entrypoint.test.js
@@ -1,2 +1,1 @@
 import assert from 'assert'
-
