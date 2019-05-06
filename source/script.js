1cce4f6421f52054d9464b103710a6f551ef80e6
Dr. Safi <sfmissive@gmail.com>
2019-05-06 23:05:57 +0300

    build

--- source/script.js
+++ source/script.js
@@ -1,16 +1,42 @@
-export function decorator({
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var _exportNames = {
+  decorator: true
+};
+exports.decorator = decorator;
+
+var _mixwith = require("mixwith");
+
+Object.keys(_mixwith).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _mixwith[key];
+    }
+  });
+});
+
+function decorator({
   /** mixin complies with `Mixin` function of the 'mixwith' module.
    * e.g. Mixin({ Superclass: class{} } => class Y extends Superclass {})
    */
-  mixin = null,
+  mixin = null
 }) {
   return Class => {
     // add controller methods for the specific module that uses them.
     if (mixin) {
-      Class = mixin({ Superclass: Class }) /* return Specific implementation Controller */
+      Class = mixin({
+        Superclass: Class
+      });
+      /* return Specific implementation Controller */
     } // else return Reusable nested unit
-    return Class
-  }
-}
 
-export * from 'mixwith'
+
+    return Class;
+  };
+}
